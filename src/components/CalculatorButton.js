c3de8db92e999384e0cc0b74bc56344c432ab700
Nahom <zerihunnahom@gmail.com>
2023-03-21 17:57:56 +0300

    Modify CalculatorButton to add proptype and className

--- src/components/CalculatorButton.js
+++ src/components/CalculatorButton.js
@@ -1,4 +1,11 @@
+import PropTypes from 'prop-types';
+
 const CalculatorButton = (props) => {
-  return <button type="button"></button>;
+  const { value } = props;
+  return (<button type="button" className="calculator-button">{value}</button>);
 };
 export default CalculatorButton;
+
+CalculatorButton.propTypes = {
+  value: PropTypes.string.isRequired,
+};
