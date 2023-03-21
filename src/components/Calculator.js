c3d763e7842632737d2b26d18b7134c1fcec160f
Nahom <zerihunnahom@gmail.com>
2023-03-21 18:36:46 +0300

    Modify Calculator component

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -1,30 +1,44 @@
 import CalculatorButton from './CalculatorButton';
 
-const Calculator = () => (
-
-  <section className="calculator">
-    <input type="number" placeholder="0" className="calculator-input" />
-    <div className="button-container">
-      <CalculatorButton value="AC" />
-      <CalculatorButton value="+/-" />
-      <CalculatorButton value="%" />
-      <CalculatorButton value="/" />
-      <CalculatorButton value="7" />
-      <CalculatorButton value="8" />
-      <CalculatorButton value="9" />
-      <CalculatorButton value="x" />
-      <CalculatorButton value="4" />
-      <CalculatorButton value="5" />
-      <CalculatorButton value="6" />
-      <CalculatorButton value="-" />
-      <CalculatorButton value="1" />
-      <CalculatorButton value="2" />
-      <CalculatorButton value="3" />
-      <CalculatorButton value="+" />
-      <CalculatorButton value="0" />
-      <CalculatorButton value="." />
-      <CalculatorButton value="=" />
-    </div>
-  </section>
-);
+const Calculator = () => {
+  const buttonValues = [
+    { id: 1, value: 'AC' },
+    { id: 1, value: '+/-' },
+    { id: 1, value: '%' },
+    { id: 1, value: '/' },
+    { id: 1, value: '7' },
+    { id: 1, value: '8' },
+    { id: 1, value: '9' },
+    { id: 1, value: 'x' },
+    { id: 1, value: '4' },
+    { id: 1, value: '5' },
+    { id: 1, value: '6' },
+    { id: 1, value: '-' },
+    { id: 1, value: '1' },
+    { id: 1, value: '2' },
+    { id: 1, value: '3' },
+    { id: 1, value: '+' },
+    { id: 1, value: '0' },
+    { id: 1, value: '.' },
+    { id: 1, value: '=' },
+  ];
+  const buttonElements = buttonValues.map((element) => (
+    <CalculatorButton
+      key={element.id}
+      className="calculator-button"
+      value={element.value}
+    />
+  ));
+  return (
+    <section className="calculator">
+      <input
+        dir="rtl"
+        type="number"
+        placeholder="0"
+        className="calculator-input"
+      />
+      {buttonElements}
+    </section>
+  );
+};
 export default Calculator;
