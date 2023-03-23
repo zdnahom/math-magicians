36cd8d6520f118f4a69a08c7598a9804eaeae5b4
Nahom <zerihunnahom@gmail.com>
2023-03-23 16:47:01 +0300

    Add logic for Quote component

---
+++ src/components/Quote.js
@@ -0,0 +1,11 @@
+const Quote = () => {
+  return (
+    <section className="quote">
+      <p className="quote-text">
+        Go and fullfill your dreams -
+        <span className="author"> Nahom</span>
+      </p>
+    </section>
+  );
+};
+export default Quote;
