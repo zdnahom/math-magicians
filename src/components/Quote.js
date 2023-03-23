81e23515758c6833fce84c5a3c849fe0d043ad6c
Nahom <zerihunnahom@gmail.com>
2023-03-23 20:22:54 +0300

    Add error and isLoading state

--- src/components/Quote.js
+++ src/components/Quote.js
@@ -2,30 +2,47 @@ import { useEffect, useState } from 'react';
 
 const Quote = () => {
   const [quote, setQuote] = useState({});
+  const [isLoading, setIsLoading] = useState(false);
+  const [hasError, setHasError] = useState(null);
   useEffect(() => {
     const fetchQuote = async () => {
-      const res = await fetch('https://api.api-ninjas.com/v1/quotes?category=failure', {
-        headers: {
-          'X-Api-Key': 'G0vLSbdKOcOzDZKrzJaE8Q==3xEs9RAaVql19Lv5',
-        },
-      });
-      const data = await res.json();
-      setQuote(data[0]);
+      setIsLoading(true);
+      try {
+        const res = await fetch(
+          'https://api.api-ninjas.com/v1/quotes?category=learning',
+          {
+            method: 'GET',
+            headers: {
+              'X-Api-Key': 'G0vLSbdKOcOzDZKrzJaE8Q==3xEs9RAaVql19Lv5',
+            },
+            contentType: 'application/json',
+          },
+        );
+        const data = await res.json();
+        setQuote(data[0]);
+      } catch (error) {
+        setHasError(error.name);
+      }
+      setIsLoading(false);
     };
     fetchQuote();
-  }, []);
-
+  }, [setQuote]);
+  if (hasError) return <div>Something went wrong!</div>;
   return (
     <section className="quote">
-      <p className="quote-text">
-        {quote.quote}
-        {' '}
-        -
-        <span className="author">
+      {isLoading ? (
+        <p>Loading...</p>
+      ) : (
+        <p className="quote-text">
+          {quote.quote}
           {' '}
-          {quote.author}
-        </span>
-      </p>
+          -
+          <span className="author">
+            {' '}
+            {quote.author}
+          </span>
+        </p>
+      )}
     </section>
   );
 };
