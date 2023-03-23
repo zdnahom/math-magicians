37fe6cd8c4a788918344aa65757bc804590ae158
Nahom <zerihunnahom@gmail.com>
2023-03-23 17:53:23 +0300

    Fetch data from ninja api

--- src/components/Quote.js
+++ src/components/Quote.js
@@ -1,9 +1,30 @@
+import { useEffect, useState } from 'react';
+
 const Quote = () => {
+  const [quote, setQuote] = useState({});
+  useEffect(() => {
+    const fetchQuote = async () => {
+      const res = await fetch('https://api.api-ninjas.com/v1/quotes?category=failure', {
+        headers: {
+          'X-Api-Key': 'G0vLSbdKOcOzDZKrzJaE8Q==3xEs9RAaVql19Lv5',
+        },
+      });
+      const data = await res.json();
+      setQuote(data[0]);
+    };
+    fetchQuote();
+  }, []);
+
   return (
     <section className="quote">
       <p className="quote-text">
-        Go and fullfill your dreams -
-        <span className="author"> Nahom</span>
+        {quote.quote}
+        {' '}
+        -
+        <span className="author">
+          {' '}
+          {quote.author}
+        </span>
       </p>
     </section>
   );
