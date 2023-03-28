19fbdc34c6bfddd50709147a1b31f195980936d4
Nahom <zerihunnahom@gmail.com>
2023-03-28 16:18:55 +0300

    Add routing functionality

--- src/App.js
+++ src/App.js
@@ -1,13 +1,29 @@
+import {
+  BrowserRouter, Routes, Route, Link,
+} from 'react-router-dom';
 import './App.css';
 import Calculator from './components/Calculator';
 import Quote from './components/Quote';
 
 function App() {
   return (
-    <div className="App">
-      <Calculator />
-      <Quote />
-    </div>
+    <BrowserRouter>
+      <header className="header">
+        <nav className="nav">
+          <ul className="navbar">
+            <li className="nav-link"><Link to="/">Home</Link></li>
+            <li className="nav-link"><Link to="/calculator">Calculator</Link></li>
+            <li className="nav-link"><Link to="/quote">Quote</Link></li>
+          </ul>
+        </nav>
+      </header>
+      <main className="App">
+        <Routes>
+          <Route path="/calculator" element={<Calculator />} />
+          <Route path="/quote" element={<Quote />} />
+        </Routes>
+      </main>
+    </BrowserRouter>
   );
 }
 
